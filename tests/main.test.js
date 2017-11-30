a78d381b5feda8fe70779d93e250394feb463a10
julio Cousteau <juliocousteau@gmail.com>
2017-11-29 23:35:44 -0200

    First Commit

---
+++ tests/main.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import { toArray } from '../src/main'
+
+describe('Smoke tests', () => {
+  it('should toArray exists', () => {
+    expect(toArray).to.exist
+  })
+})
+
